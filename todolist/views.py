d3f40592c2887c97e912e4e295e0cf666a4b9c4a
dkhsabjsk <01037150928k@gmil.com>
2022-08-09 16:08:12 +0900

    0809

---
+++ todolist/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from todolist.models import Todo
+from django.utils import timezone
+
+# Create your views here.
+def index(request):
+    todo_list = Todo.objects.all()
+    context = {'todo_list': todo_list}
+    return render(request, 'todolist/index.html', context)
+def delete(request, todo_id):
+    delete_id = get_object_or_404(Todo, pk=todo_id)
+    delete_id.delete()
+    return redirect('/todolist/')
+def add(request):
+    q = Todo(todo=request.POST.get('todo'), is_complete='0', create_date=timezone.now())
+    q.save()
+    return redirect('/todolist/')
